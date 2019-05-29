8dacfed3e6d45861a9e117f1e4fea05715212b70
Jaimi <jaimipinnock@gmail.com>
2019-05-29 14:55:03 -0600

    Split up checkout process
    Split checkout process in to three methods.
    Step one begins checkout process.
    Step two fills out personal data on order page and submits form
    Step three hits confirms and places the order.

--- page_functionality/checkout_functionality.rb
+++ page_functionality/checkout_functionality.rb
@@ -1,13 +1,17 @@
 class Checkout
 
-  def checkout_process(first_name = "Standard", last_name = "User", postal_code = "84101")
+  def checkout_step_one
     $browser.get "https://www.saucedemo.com/cart.html"
     $browser.find_element(:link_text, 'CHECKOUT').click
+  end
+  def checkout_step_two(first_name = "Standard", last_name = "User", postal_code = "84101")
     $wait.until{$browser.find_element(:id, 'first-name').displayed?}
     $browser.find_element(:id, 'first-name').send_keys(first_name)
     $browser.find_element(:id, 'last-name').send_keys(last_name)
     $browser.find_element(:id, 'postal-code').send_keys(postal_code)
     $browser.find_element(:id, 'postal-code').submit
+  end
+  def checkout_step_three
     $browser.find_element(:link_text, "FINISH").click
   end
 
