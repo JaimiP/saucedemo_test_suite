ad45c3be3677425d0640e90f0be33e4f98b1d0e5
Jaimi <jaimipinnock@gmail.com>
2019-06-11 16:23:54 -0600

    Created Reporting
    Created reporting. 
    Added cucumber.yml that will create HTML reports for these tests.
    Updated env.rb to create the correct sorted file paths for this reporting.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,13 @@
 require 'selenium-webdriver'
 require 'rspec'
+require 'yaml'
+require 'fileutils'
+
+year_month_folder = Time.now.strftime('%Y_%m')
+day_folder = Time.now.strftime('%Y_%m_%d')
+results_file_path = File.expand_path("./test_results/#{year_month_folder}/#{day_folder}")
+
+FileUtils.mkdir_p(results_file_path)
 
 Before do |scenario|
  puts scenario.name
